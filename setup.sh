a16b823b74c6b8a824e352e959bb56e7cc2fa5ff
Jingxin Feng <Jingxin.Feng@netapp.com>
2014-11-07 14:57:15 -0800

    fix

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ HADOOP_PACKAGE=hadoop-2.4.1
 HBASE_PACKAGE=hbase-0.98.7-hadoop2
 
 # Setup the Hadoop packages
-#cp -rf packages/${HADOOP_PACKAGE} hadoop
-#cp -rf packages/${HBASE_PACKAGE} hbase
+cp -rf packages/${HADOOP_PACKAGE} hadoop
+cp -rf packages/${HBASE_PACKAGE} hbase
 
 # Copy the configuration files
 mkdir /opt/mambo/mambo-ec2-deploy/hadoop/conf
