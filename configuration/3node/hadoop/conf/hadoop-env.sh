f80a26b21ca10b903969054ac8ba2d87c6321e81
Jingxin Feng <Jingxin.Feng@netapp.com>
2014-11-07 14:20:28 -0800

    dir change

---
+++ configuration/3node/hadoop/conf/hadoop-env.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr/lib/jvm/jre
+export HADOOP_PREFIX=/opt/mambo/mambo-ec2-deploy/hadoop
--- configuration/hadoop/conf/hadoop-env.sh
+++
@@ -1,2 +0,0 @@
-export JAVA_HOME=/usr/local/java/jdk1.8.0_20/
-export HADOOP_PREFIX=/x/eng/rtpbld00/scratch/jingxin/hadoop-2.4.1/
